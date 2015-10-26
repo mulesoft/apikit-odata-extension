bfbe9d76083fcc3bdddd4610a4a400a564334cee
mcoaker <mhcoaker@gmail.com>
2015-10-26 19:22:42 -0300

    removing unneeded test

--- src/test/java/org/mule/module/apikit/odata/processor/ODataUriParserTestCase.java
+++ src/test/java/org/mule/module/apikit/odata/processor/ODataUriParserTestCase.java
@@ -40,21 +40,6 @@ public class ODataUriParserTestCase {
 	}
 
 	@Test
-	public void sendingXMLAsBodyButJsonAsContentTypeThrowsException() {
-		try {
-			String xml = "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>"
-					+ "<entry xmlns:d=\"http://schemas.microsoft.com/ado/2007/08/dataservices\" xmlns:m=\"http://schemas.microsoft.com/ado/2007/08/dataservices/metadata\" xmlns=\"http://www.w3.org/2005/Atom\">"
-					+ "  <title />" + "  <updated>2013-09-18T23:46:19.3857256Z</updated>" + "  <author>" + "    <name />" + "  </author>" + "  <id />"
-					+ "  <content type=\"application/xml\">" + "    <m:properties>" + "      <d:OrderID>20000</d:OrderID>" + "      <d:ShipName>ship-name-1</d:ShipName>"
-					+ "      <d:ShipAddress>ship-address-1</d:ShipAddress>" + "    </m:properties>" + "  </content>" + "</entry>";
-
-			BodyToJsonConverter.convertPayload(false, xml);
-			fail("Exception expected");
-		} catch (ODataInvalidFormatException e) {
-		}
-	}
-
-	@Test
 	public void sendingJSONAsBodyButXMLasContentTypeThrowsException() {
 		try {
 			String input = "{\"OrderID\":20000,\"ShipAddress\":\"ship-address-1\",\"ShipName\":\"ship-name-1\"}";
