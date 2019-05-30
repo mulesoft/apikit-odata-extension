aecdb7ed2d5a87de27ab5f4f3f92cf836648c9e0
Juan Brasca <juan.brasca@gmail.com>
2019-05-30 18:10:40 -0300

    ODATA-103: Fix issue when path to raml contains blank spaces (#50)

--- src/main/java/org/mule/module/apikit/odata/metadata/OdataMetadataManagerImpl.java
+++ src/main/java/org/mule/module/apikit/odata/metadata/OdataMetadataManagerImpl.java
@@ -8,17 +8,10 @@ package org.mule.module.apikit.odata.metadata;
 
 import org.apache.log4j.Logger;
 import org.mule.module.apikit.model.AMFWrapper;
-import org.mule.module.apikit.odata.context.OdataContextVariables;
-import org.mule.module.apikit.odata.exception.ODataException;
-import org.mule.module.apikit.odata.metadata.exception.OdataMetadataEntityNotFoundException;
 import org.mule.module.apikit.odata.metadata.exception.OdataMetadataFieldsException;
 import org.mule.module.apikit.odata.metadata.exception.OdataMetadataFormatException;
-import org.mule.module.apikit.odata.metadata.model.entities.EntityDefinition;
-import org.mule.module.apikit.odata.metadata.model.entities.EntityDefinitionProperty;
 import org.mule.module.apikit.odata.metadata.model.entities.EntityDefinitionSet;
-import java.util.ArrayList;
-import java.util.List;
-import static org.mule.module.apikit.model.Entity.pluralizeName;
+import java.net.URLDecoder;
 
 public class OdataMetadataManagerImpl extends OdataMetadataManager {
   private final EntityDefinitionSet entitySet;
@@ -27,7 +20,7 @@ public class OdataMetadataManagerImpl extends OdataMetadataManager {
   public OdataMetadataManagerImpl(String ramlPath) throws OdataMetadataFormatException {
     logger.info("Initializing Odata Metadata");
     try {
-      AMFWrapper apiWrapper = new AMFWrapper(ramlPath);
+      AMFWrapper apiWrapper = new AMFWrapper(URLDecoder.decode(ramlPath));
       entitySet = apiWrapper.getSchemas();
     } catch (OdataMetadataFieldsException e) {
       throw new OdataMetadataFormatException(e.getMessage());
