d443bc751dbd95ab0c34f3896bb7d95a8d9f2efc
Juan Brasca <juan.brasca@gmail.com>
2019-12-06 16:25:09 -0300

    APIKIT-2158: Add MTF tests to OData MySQL example (#59)

--- src/test/java/org/mule/module/apikit/model/ODataScaffolderServiceTestCase.java
+++ src/test/java/org/mule/module/apikit/model/ODataScaffolderServiceTestCase.java
@@ -17,7 +17,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import java.io.File;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
