9a937d360e60c135cfb406a1f0e9afb25251a39f
Dhairya Shah <dhairya.shah@salesforce.com>
2025-03-28 11:01:33 +0530

    W-18104617: Add getter and setter methods for OdataContextVariables

--- src/main/java/org/mule/module/apikit/odata/context/OdataContextVariables.java
+++ src/main/java/org/mule/module/apikit/odata/context/OdataContextVariables.java
@@ -10,13 +10,37 @@ import java.util.List;
 
 public class OdataContextVariables {
 
-  public String remoteEntityName;
-  public String keyNames;
-  public List<String> fields;
+  private String remoteEntityName;
+  private String keyNames;
+  private List<String> fields;
 
   public OdataContextVariables(String remoteEntityName, String keyNames, List<String> fields) {
     this.remoteEntityName = remoteEntityName;
     this.keyNames = keyNames;
+    this.fields = fields;
+  }
+
+  public String getRemoteEntityName() {
+    return remoteEntityName;
+  }
+
+  public void setRemoteEntityName(String remoteEntityName) {
+    this.remoteEntityName = remoteEntityName;
+  }
+
+  public String getKeyNames() {
+    return keyNames;
+  }
+
+  public void setKeyNames(String keyNames) {
+    this.keyNames = keyNames;
+  }
+
+  public List<String> getFields() {
+    return fields;
+  }
+
+  public void setFields(List<String> fields) {
     this.fields = fields;
   }
 }
